e09949ca5f6d8d89cc8154ea69e95ec5caf91a8f
Jesse Moniz <jmoniz155@icloud.com>
2021-10-25 09:33:39 -0700

    troubleshoot schema server.js

--- db/schema.sql
+++ db/schema.sql
@@ -21,16 +21,32 @@ CREATE TABLE role (
 
 -- Create Employee -- 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY(manager_id)
-    REFERENCES employee(id)
-    FOREIGN KEY(role_id)
-    REFERENCES KEY (employees_db)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
+
+
+
+
+-- CREATE TABLE employee (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INT NOT NULL,
+--     FOREIGN KEY (role_id)
+--     REFERENCES role(id)
+--     manager_id INT,
+--     FOREIGN KEY (manager_id)
+--     REFERENCES employee(id)
+    
+-- );
 
 
 
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 // TODO: Import and require mysql2
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const util = require("util");
 
-const PORT = process.env.PORT || 3001;
-const app = inquirer();
+//const PORT = process.env.PORT || 3001;
+//const app = inquirer;
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
-// TODO: Connect to database
-const db = mysql.createConnection(
+
+const connection = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
@@ -18,4 +16,108 @@ const db = mysql.createConnection(
     database: 'employees_db',
   },
 );
+
+function viewAllDept(answer) {
+  connection.promise().query("SELECT * FROM departments;")
+    .then( ([rows]) => {
+      console.table(rows);
+    }).then(() => {generalMenu()})
+    .catch(console.log);
+};
+
+function viewAllRoles(answer) {
+  connection.promise().query("SELECT * FROM role;")
+          .then( ([rows]) => {
+            console.table(rows);
+          }).then(() => {generalMenu()})
+          .catch(console.log);
+          
+};
+
+function viewAllEmployees(answer) {
+  connection.promise().query(`select
+  a.id,
+  CONCAT(a.first_name, " ", a.last_name) AS name,
+  CONCAT(b.first_name, " ", b.last_name) AS manager
+from employee AS a LEFT JOIN employee AS b
+on a.manager_id = b.id;;`)
+          .then( ([rows]) => {
+            console.table(rows);
+          }).then(() => {generalMenu()})
+          .catch(console.log);
+};
+
+function deptAdd(answer) {
+  inquirer.prompt(
+    [ {
+      type: "input",
+      name: "deptName",
+      message: "What is the name of the Department?",
+    }]
+  ).then((answer) => {
+    connection.promise().query(`insert into department (name) values ("${answer.deptName}");`)
+    .then(() => { console.log("Department added!")})
+    .then(() => {generalMenu()})
+  });
+};
+
+function generalMenu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "generalMenu",
+        message: "What would you like to do?",
+        choices: ["View All Departments", 
+        "View All Roles", 
+        "View All Employees", 
+        "Add a Department", 
+        "Add a Role", 
+        "Add an Employee", 
+        "Update an Employee Role",
+      "Exit App"],
+      },
+    ])
+
+    .then((answer) => {
+      switch(answer.generalMenu) {
+        case "View All Departments": viewAllDept(answer.generalMenu);
+          break;
+        case "View All Roles": viewAllRoles(answer.generalMenu);
+          break;
+        case "View All Employees": viewAllEmployees(answer.generalMenu);
+          break;
+        case "Add a Department": deptAdd(answer.generalMenu);
+          break;
+        case "Add a Role":
+          break;
+        case "Add an Employee":
+          break;
+        case "Update an Employee Role":
+          break;
+        case "Exit App": process.exit();
+      }
+    });
+}
+
+generalMenu();
+
+
+
+
+
+
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
+
 
