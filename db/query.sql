ace193e7b14f701fcad4bc480f18e9da0cc54150
Jesse Moniz <jmoniz155@icloud.com>
2021-10-26 14:50:55 -0700

    update db

--- db/query.sql
+++ db/query.sql
@@ -1,8 +1,12 @@
 SELECT  first_name, last_name, role_id from employee;
 
 SELECT
-    a.id,
-    a.first_name,
-    a.last_name,
+  a.id,
+  CONCAT(a.first_name, " ", a.last_name) AS name,
+  CONCAT(b.first_name, " ", b.last_name) AS manager
+from employee AS a LEFT JOIN employee AS b
+on a.manager_id = b.id;
 
-FROM employee AS a JOIN employee ON 
+select * from department;
+
+SELECT CONCAT(employee.first_name, " ", employee.last_name) AS name FROM employee;
